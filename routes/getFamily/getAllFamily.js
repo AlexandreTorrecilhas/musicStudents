8d63dff3ef2568b57f17de3fdea436305fc41658
AlexandreTorrecilhas <69021088+AlexandreTorrecilhas@users.noreply.github.com>
2024-01-31 12:50:01 -0300

    Route to get all document were created

--- routes/getFamily/getAllFamily.js
+++ routes/getFamily/getAllFamily.js
@@ -2,3 +2,22 @@ const {connectToMongoDB, getDB} = require ('../../dbConnection/connectionFile.js
 const express = require('express');
 const app = express();
 
+connectToMongoDB().catch((error) =>{
+    console.log("Connection falied: ", error)
+    process.exit(1);
+})
+
+app.get('/instrumentFamily', async (req, res) => {
+    try {
+        const db = getDB();
+        const document = await db.collection("instrumentFamily").find().toArray();
+        res.status(200).json(document)
+    } catch (error) {
+        console.log("Error when trying to fetch the documents", error);
+        res.status(500).json({error: "Could fetch the documents"});
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Running on port 3000")
+})
