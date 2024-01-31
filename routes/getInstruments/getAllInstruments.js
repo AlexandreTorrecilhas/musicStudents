e5520f7ef88317fd2aabd908898cbe70a098a6e3
AlexandreTorrecilhas <69021088+AlexandreTorrecilhas@users.noreply.github.com>
2024-01-31 12:03:16 -0300

    adding code

---
+++ routes/getInstruments/getAllInstruments.js
@@ -0,0 +1,10 @@
+const {connectToMongoDB,getDB} = require('../../dbConnection/connectionFile');
+const express = require('express');
+const app = express();
+
+connectToMongoDB().catch((error) => {
+    console.log("Connection failed: ". error);
+    process.exit(1)
+});
+
+app.get('/instruments', async (req, ))
