67db25da6856b67cec9c9fbccd6654add145e4f8
AlexandreTorrecilhas <69021088+AlexandreTorrecilhas@users.noreply.github.com>
2024-02-01 12:09:24 -0300

    Adding the project by git

--- routes/getInstruments/getAllInstruments.js
+++ routes/getInstruments/getAllInstruments.js
@@ -2,9 +2,24 @@ const {connectToMongoDB,getDB} = require('../../dbConnection/connectionFile');
 const express = require('express');
 const app = express();
 
+const defaultDoor = 3000;
+
 connectToMongoDB().catch((error) => {
     console.log("Connection failed: ", error);
     process.exit(1)
 });
 
-app.get('/instruments', async (req, ))
+app.get('/instruments', async (req, res) => {
+    try {
+        const db = getDB();
+        const document = await db.collection("instrument").find().toArray();
+        res.status(200).json(document);
+    } catch (error) {
+        console.log("Error when tried to connect");
+        res.status(500).json({error: "Error when tried to fetch the document"})
+    }
+})
+
+app.listen(defaultDoor, ()=> {
+    console.log(`Runing on port ${defaultDoor}`)
+})
--- routes/getParents/getAllParents.js
+++ routes/getParents/getAllParents.js
@@ -1,3 +1,25 @@
 const {connectToMongoDB, getDB} = require("../../dbConnection/connectionFile");
 const express = require("express");
 const app = express();
+
+const defaultDoor = 3000;
+
+connectToMongoDB().catch((error) => {
+    console.log("Connection failed: ", error);
+    process.exit(1);
+})
+
+app.get('/parents', async (req, res) => {
+    try {
+        const db = getDB();
+        const document = await db.collection("parents").find().toArray();
+        res.status(200).json(document);
+    } catch (error) {
+        console.log("Error when tried to fetch the documents");
+        res.status(500).json({error: "Error when tried to fetch the documents"})
+    }
+})
+
+app.listen(defaultDoor, () =>{
+    console.log(`Listing on port ${defaultDoor}`)
+})
