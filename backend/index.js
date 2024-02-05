f10f59b2f3e575cd8c73bf6f61956ad75f7624df
AlexandreTorrecilhas <69021088+AlexandreTorrecilhas@users.noreply.github.com>
2024-02-05 13:07:07 -0300

    All the routes importation were transfered to the file route.js into the folder routes

--- backend/index.js
+++ backend/index.js
@@ -5,17 +5,10 @@ dotenv.config({path: dotenvPath})
 
 const express = require('express');
 const app = express();
-
-//Importing the routes
-const getAllStudents = require('./routes/getStudent/getAllStudents');
-const getAllFamily = require('./routes/getFamily/getAllFamily');
-const getAllInstruments = require("./routes/getInstruments/getAllInstruments");
-const getAllParents = require('./routes/getParents/getAllParents')
-
-
+const routes = require('./routes/routes')
 
 //Routes
-app.use('/index', getAllStudents, getAllFamily, getAllInstruments, getAllParents);
+app.use('/index', ...routes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Running on port ${process.env.PORT}`)
---
+++ backend/routes/routes.js
@@ -0,0 +1,6 @@
+const getAllStudents = require('./getStudent/getAllStudents');
+const getAllFamily = require('./getFamily/getAllFamily');
+const getAllInstruments = require("./getInstruments/getAllInstruments");
+const getAllParents = require('./getParents/getAllParents');
+
+module.exports=[getAllStudents, getAllFamily, getAllInstruments, getAllParents]
